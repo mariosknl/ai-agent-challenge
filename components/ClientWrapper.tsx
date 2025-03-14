e212c60fea3f288148e768ecfbd4a00900d2a300
mariosknl <mariosknl@outlook.com>
2025-03-14 21:03:12 +0200

    Add Schematic integration with new components and dependencies
    - Added `@schematichq/schematic-react` as a dependency in `package.json`.
    - Updated `pnpm-lock.yaml` to include new schematic packages.
    - Enhanced `ClientWrapper` to include `SchematicProvider` and handle publishable key validation.
    - Created `SchematicWrapped` component to manage user identification with Schematic events.

--- components/ClientWrapper.tsx
+++ components/ClientWrapper.tsx
@@ -1,11 +1,26 @@
 "use client";
 
 import { ClerkProvider } from "@clerk/nextjs";
+import { SchematicProvider } from "@schematichq/schematic-react";
+import SchematicWrapped from "./SchematicWrapped";
 
 export default function ClientWrapper({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  return <ClerkProvider>{children}</ClerkProvider>;
+  const schematicPubKey = process.env.NEXT_PUBLIC_SCHEMATIC_PUBLISHABLE_KEY;
+  if (!schematicPubKey) {
+    throw new Error(
+      "No Schematic Publishable Key found. Please add it to your .env.local file."
+    );
+  }
+
+  return (
+    <ClerkProvider>
+      <SchematicProvider publishableKey={schematicPubKey}>
+        <SchematicWrapped>{children}</SchematicWrapped>
+      </SchematicProvider>
+    </ClerkProvider>
+  );
 }
